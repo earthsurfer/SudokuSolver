1ae5d5651ad0bbf9a06378b216ca4ce8e7ac9197
earthsurfer <C00lieM0nster>
2015-09-17 17:15:12 -0600

    Adding python files
    Actual python scripts

---
+++ sudokuSolver.py
@@ -0,0 +1,185 @@
+#!/usr/bin/python -tt
+## 
+## SOUDOKU Solver Project 
+##  Terry Norton
+##
+import sys
+import os
+import time
+
+def inRow(i,j):     return ( int(i)/9 == int(j)/9 )
+def inCol(i,j):  return ( int(i) % 9 == int(j) % 9 )  
+def inBlk(i,j):   return (i/27 == j/27) and ( i%9/3 == j%9/3 )
+
+def prDebug (outStr):
+  """ Simple function to print debug string when global:outputDebug = True
+  """
+  if outputDebug: print outStr
+  return
+  
+def prVerbose (outStr):
+  """ Funtion to print status information for the solver algorithym
+  """
+  if outputVerbose: print outStr
+  return
+
+def readPuzzle(file):
+  """ readPuzzle(file):
+        function to read in puzzle file of 9 lines of 9 characters each 
+        returns an 80 character string
+        The return data structure will evolve over time.
+  """
+  try:
+    f=open(file)
+    prDebug('readPuzzle: Opening puzzle file: ' + file)
+  except IOError:
+    prDebug('readPuzzle: Failed opening puzzle file')
+    print 'IOError on: '+file
+    return 1
+  a = []
+  a=f.read()
+  f.close()
+  a=a.replace('\n','').replace(' ','').replace(',','')
+  prDebug ('a:'+a )
+  return a
+  
+def solvePuzzle(sudoku):
+  """ sudoku puzzle solver
+        INPUT: sudoku - 80 character string
+        RETURN: 80 character string of solved puzzle
+  """
+  ## this is a stolen algorithm rewritten for my comprehension
+  global loopCntr
+  global solved
+  # find first 0 element
+  prDebug ( 'DEBUG001:  call:'+ sudoku  )
+  loopCntr += 1
+  if loopCntr == 2000000: 
+    print 'Max Loops Reached: '+str(loopCntr)
+    sys.exit(100)
+  if '0' in sudoku:
+    i = sudoku.index('0')
+  else:
+    ## nothing more to solve!
+    ## this code is depricated since the return path was changed
+    ## to more elegantly return from the recursion.
+    Done = True
+    prDebug ('Solved!')
+    prDebug (sudoku)
+    return sudoku
+  
+  ## collect excluded numbers for i
+  excludedNums = set()
+  for j in range(len(sudoku)):
+    if inRow(i,j) or inCol(i,j) or inBlk(i,j):
+      excludedNums.add(sudoku[j])
+      # prDebug( 'DEBUG002: i:' + str(i) + ' ' + sudoku[i] 
+      #   + ' j:' + str(j) + ' ' + sudoku[j] 
+      #   + ' excludedNums:' + str(excludedNums) )
+  # prDebug( 'DEBUG003: i:' + str(i) + ' j:' + str(j) + ' excludedNums:' + str(excludedNums) )
+  
+  ## Now attempt a substitution
+  for trial in '123456789':
+    if trial not in excludedNums:
+      # prDebug( 'DEBUG004: Trail solution - i:' + str(i) 
+      #   + ' trial value:' + str(trial) )
+      # prDebug ( 'DEBUG005: trial:'+ sudoku[:i] + trial + sudoku[i+1:] )
+      tmpSolution = solvePuzzle( sudoku[:i] + trial + sudoku[i+1:] )
+      if '0' not in tmpSolution: 
+        if not solved:
+          # prDebug('DEBUG009: Solved! loopCntr: '+str(loopCntr))
+          prVerbose('SOLVER: Solved! loopCntr: '+str(loopCntr))
+          # prDebug(tmpSolution)
+          prVerbose(tmpSolution)
+        solved = True
+        return tmpSolution
+  return sudoku
+
+def printPuzzle ( sudoku ):
+  print '-'*34
+  print '|',
+  for i in range( len(sudoku) ):
+    if sudoku[i] != '0': print ' ' + sudoku[i],
+    else: print ' ' + '*',
+    if not ((i+1) % 9): print '\n',
+    if not ((i+1) % 27): print '-'*34 
+    if not ((i+1) % 3): print '|',
+  return
+  
+global outputDebug
+global outputVerbose
+outputDebug = False
+outputVerbose = False
+loopCntr = 0
+solved = False
+
+
+# Define a main() function that prints a little greeting.
+def main():
+  """ Solver for basic sudoku puzzles. Input to be either space or comma
+      delimited. 0 represents an unfilled / solved location. 
+      All 81 locations must be present.
+      Ex 1:
+        0 3 0 6 2 0 0 0 0
+        0 0 6 0 0 1 7 0 2
+        0 0 0 9 0 0 3 1 0
+        5 9 0 0 0 4 0 0 8
+        7 8 2 0 0 0 9 5 4 
+        4 0 0 8 0 0 0 3 7
+        0 7 9 0 0 2 0 0 0
+        3 0 5 4 0 0 6 0 0
+        0 0 0 0 9 6 0 2 0
+        
+      COMMAND LINE:
+      
+        sudoku.vv.py [--debug][--v] puzzle.txt
+      
+      Options are:
+        --debug   Verbose debugging output to stdout
+        --v       Verbose solver information - outputs intermediate computations
+  """
+  global outputDebug
+  global outputVerbose
+  global loopCntr
+  global solved
+  outputDebug = False
+  outputVerbose = False
+  loopCntr = 0
+  solved = False
+
+  
+  args = sys.argv[1:]
+  for flag in args:
+    if flag == '--debug':
+      outputDebug = True
+    if flag == '--v':
+      outputVerbose = True
+  if outputDebug: del (args[args.index('--debug')])
+  if outputVerbose: del (args[args.index('--v')])
+  prDebug('outputDebug is ON')
+  prVerbose ('outputVerbose is ON')
+  prDebug (str(args))
+  
+  sudoku = ''
+  ## read in puzzle file
+  sudoku = readPuzzle(args[0])
+  prDebug (sudoku)
+  prDebug ( 'main: len(sudoku)=' + str(len(sudoku)) )
+  ## print out input puzzle
+  print 'Input Puzzle:'
+  printPuzzle( sudoku )
+  
+  startTime = time.time()
+  solvedPuzzle = solvePuzzle( sudoku )
+  stopTime = time.time()
+  
+  print '\nSolved Puzzle:'
+  printPuzzle( solvedPuzzle )
+  prVerbose('Solve time: '+str(stopTime-startTime)+' sec.')
+  sys.exit(0)
+  
+  
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
